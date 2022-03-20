145cb13e339d7448608734b0dd9445174e9b83c1
graceclaudia19 <graceclaudia78@gmail.com>
2022-03-21 04:02:35 +0700

    BFS done

--- src/dagorlz/searching.cs
+++ src/dagorlz/searching.cs
@@ -51,15 +51,54 @@ namespace dagorlz
             }
         }
 
+        public static string[] BFS(string start, string fileToSearch, bool checkAllOccur){
+            bool found = false;
+            Queue<string> q = new Queue<string>();
+            List<string> output = new List<string>();
+
+            q.Enqueue(start);
+
+            while(q.Count>0){
+                string startDir = q.Dequeue();
+                output.Add(startDir);
+
+                string[] files = Directory.GetFiles(startDir);
+                foreach(string file in files){
+                    output.Add(file);
+                    if (file.Contains(fileToSearch)){
+                        found = true;
+                        output.Add("?");
+                    }
+                }
+                if (found && !checkAllOccur){
+                    break;
+                }
+                string[] otherDir = Directory.GetDirectories(startDir);
+                foreach (string dir in otherDir){
+                    res.Enqueue(dir);
+                }
+            }
+            return output.ToArray();
+        }
+
+
+
         // for testing purpose
         public static void Main(string[] args)
         {
-            // DFS testing
-            string[] resultDFS = DFS("testFolder", "found.txt", false);
-            foreach (string res in resultDFS)
-            {
-                Console.WriteLine(res);
-            }
+            // // DFS testing
+            // string[] resultDFS = DFS("testFolder", "found.txt", false);
+            // foreach (string res in resultDFS)
+            // {
+            //     Console.WriteLine(res);
+            // }
+
+            // // BFS testing
+            // string[] resultBFS = BFS("testFolder", "found.txt", true);
+            // foreach (string res in resultBFS)
+            // {
+            //     Console.WriteLine(res);
+            // }
         }
 
     }
